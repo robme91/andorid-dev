ec2aef5893b6754ed376db4ab561e6b350c2e6dc
Robin Mehlitz <robin.mehlitz@beuth-hochschule.de>
2015-11-15 21:02:54 +0100

    created 2 activities and created person data text and edit views. First activity creates second and second saves data which the first can use

--- TwoActivitiesExercise/app/src/main/java/de/beuth_hochschule/s790642/twoactivitiesexercise/InsertScreen.java
+++ TwoActivitiesExercise/app/src/main/java/de/beuth_hochschule/s790642/twoactivitiesexercise/InsertScreen.java
@@ -4,18 +4,58 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class InsertScreen extends AppCompatActivity {
+    private EditText editName;
+    private EditText editAge;
+    private EditText editGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_screen);
+
+        editName = (EditText) findViewById(R.id.insert_screen_edit_name);
+        editAge = (EditText) findViewById(R.id.insert_screen_edit_age);
+        editGender = (EditText) findViewById(R.id.insert_screen_edit_gender);
+
+        editName.setText(R.string.insertScreen_edit_name);
+        editAge.setText(R.string.insertScreen_edit_age);
+        editGender.setText(R.string.insertScreen_edit_gender);
+
+        editName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    editName.getText().clear();
+                }
+            }
+        });
+        editAge.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    editAge.getText().clear();
+                }
+            }
+        });
+        editGender.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    editGender.getText().clear();
+                }
+            }
+        });
     }
 
-    public void onBackBtnClick(View view){
+
+    public void onSaveBtnClick(View view){
         Intent intent = getIntent();
-        intent.putExtra("ergebnis", "Das Ergebnis");
+        intent.putExtra(getString(R.string.insertScreen_bundle_name), editName.getText().toString());
+        intent.putExtra(getString(R.string.insertScreen_bundle_age), editAge.getText().toString());
+        intent.putExtra(getString(R.string.insertScreen_bundle_gender), editGender.getText().toString());
         setResult(RESULT_OK, intent);
     }
 }
--- TwoActivitiesExercise/app/src/main/java/de/beuth_hochschule/s790642/twoactivitiesexercise/MainActivity.java
+++ TwoActivitiesExercise/app/src/main/java/de/beuth_hochschule/s790642/twoactivitiesexercise/MainActivity.java
@@ -5,25 +5,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView authorView;
+    private TextView nameView;
+    private TextView ageView;
+    private TextView genderView;
+    final int activity_insert_screen = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authorView = (TextView) findViewById(R.id.main_textView_author);
+        authorView.setText(R.string.main_textView_author);
+
+        nameView = (TextView) findViewById(R.id.Main_TextView_Name);
+        ageView = (TextView) findViewById(R.id.Main_TextView_Alter);
+        genderView = (TextView) findViewById(R.id.main_textView_gender);
+
+        nameView.setText(R.string.main_textView_name);
+        ageView.setText(R.string.main_textView_alter);
+        genderView.setText(R.string.main_textView_gender);
     }
 
     public void onBtnClick(View view){
         //Aufruf der anderen Activity
-        startActivityForResult(new Intent(this, InsertScreen.class), 2);
+        startActivityForResult(new Intent(this, InsertScreen.class), activity_insert_screen);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK && resultCode==2){
-            String result = data.getStringExtra("ergebnis");
+        if(resultCode == Activity.RESULT_OK && requestCode == activity_insert_screen){
+            Bundle result = data.getExtras();
+            nameView.setText(result.getString(getString(R.string.insertScreen_bundle_name)));
+            ageView.setText(result.getString(getString(R.string.insertScreen_bundle_age)));
+            genderView.setText(result.getString(getString(R.string.insertScreen_bundle_gender)));
         }
     }
 }
